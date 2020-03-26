45e9d8f5258f645d7a4c22b3d9ffaad17f6e32da
AndreWenceslau <51447462+AndreWenceslau@users.noreply.github.com>
2020-03-26 15:52:46 -0300

    Terminda os testes da classe Agencias

--- FirstTestes/Testes/AgenciasTeste.cs
+++ FirstTestes/Testes/AgenciasTeste.cs
@@ -15,35 +15,40 @@ namespace FirstTestes
         {
             context = new SistemaContext();
         }
-        private SistemaContext context;
 
 
         [TestMethod]
         public void AlterarTeste()
         {
-
-            bool Alterar(Agencia agencia)
+           var agencia = new Agencia();
+            agencia.Id = 1000;
+            agencia.Banco = "banco teste 02";
+            agencia.NomeAgencia = "banco teste 2";
+            agencia.NumeroAgencia = 5154;
+            agencia.RegistroAtivo = true;
+            bool Alterar(Agencia agencias)
             {
-
-                var agenciaOriginal = context.Agencias.FirstOrDefault(x => x.Id == 1);
-
-                if (agenciaOriginal == null)
-                    return false;
-
-                agenciaOriginal.Banco = agencia.Banco;
-                agenciaOriginal.NomeAgencia = agencia.NomeAgencia;
-                agenciaOriginal.NumeroAgencia = agencia.NumeroAgencia;
+                agencia.Banco = "banco";
+                agencia.NomeAgencia = "agencia";
+                agencia.NumeroAgencia = 51546;
                 int quantidadeAfetada = context.SaveChanges();
-                return quantidadeAfetada == 1;
-
+               return true;
             }
+            var resultado = Alterar(agencia);
+            Assert.AreEqual(true, Alterar(agencia));
         }
         [TestMethod]
         public void ApagarTeste()
         {
             bool Apagar(int id)
             {
-                var agencia = context.Agencias.FirstOrDefault(x => x.Id == 10000);
+                var agenciaApagar = new Agencia();
+                agenciaApagar.Id = 1000;
+                agenciaApagar.Banco = "banco teste 02";
+                agenciaApagar.NomeAgencia = "banco teste 2";
+                agenciaApagar.NumeroAgencia = 5154;
+                agenciaApagar.RegistroAtivo = true;
+                var agencia = agenciaApagar;
 
                 if (agencia == null)
                 {
@@ -52,37 +57,57 @@ namespace FirstTestes
 
                 agencia.RegistroAtivo = false;
                 int quantidadeAfetada = context.SaveChanges();
-                return quantidadeAfetada == 1;
+                return quantidadeAfetada == 0;
             }
+            var resultado = Apagar(1000);
+            Assert.AreEqual(true, resultado);
         }
         [TestMethod]
         public void InserirTeste()
         {
-            int Inserir(Agencia agencia)
+            var agencia = new Agencia();
+            agencia.Banco = "banco teste 02";
+            agencia.NomeAgencia = "banco teste 2";
+            agencia.NumeroAgencia = 5154;
+            agencia.RegistroAtivo = true;
+            int Inserir(Agencia agencias)
             {
                 context.Agencias.Add(agencia);
                 context.SaveChanges();
                 return agencia.Id;
             }
+            var resultado = Inserir(agencia);
+            Assert.AreEqual(agencia.Id, resultado);
         }
         [TestMethod]
         public void ObterPelodTeste()
         {
+            var agencia = new Agencia();
+            agencia.Banco = "banco teste 02";
+            agencia.NomeAgencia = "banco teste 2";
+            agencia.NumeroAgencia = 5154;
+            agencia.RegistroAtivo = true;
             Agencia ObterPeloId(int id)
             {
-                var agencia = context.Agencias.FirstOrDefault(x => x.Id == 1);
-                return agencia;
+                var agencias = agencia;
+                return agencias;
             }
+            var resultado = ObterPeloId(agencia.Id);
+            Assert.AreEqual(agencia.Id, agencia.Id);
         }
         [TestMethod]
-        public void ObterTodos()
+        public void ObterTodosTeste()
         {
             List<Agencia> ObterTodos()
             {
                 return context.Agencias.Where(x => x.RegistroAtivo == true).OrderBy(x => x.Id).ToList();
-
+               
             }
+            var resultado = ObterTodos(); 
+            
+            Assert.IsNotNull(resultado);
         }
+
     }
 
 }
