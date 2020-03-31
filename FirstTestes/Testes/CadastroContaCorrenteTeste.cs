a147bba7893515b2ede1bd0bffbf331e4b2c5b6b
AndreWenceslau <51447462+AndreWenceslau@users.noreply.github.com>
2020-03-31 20:07:36 -0300

    Corrigido erros nos testes

--- FirstTestes/Testes/CadastroContaCorrenteTeste.cs
+++ FirstTestes/Testes/CadastroContaCorrenteTeste.cs
@@ -11,79 +11,110 @@ using System.Web.Mvc;
 namespace FirstTestes.Testes
 {
     [TestClass]
-    public class CadastroContaCorrenteTeste:CadastroContaCorrenteController
+    public class CadastroContaCorrenteTeste : CadastroContaCorrenteRepository
     {
-        private CadastroContaCorrenteRepository repository;
 
         [TestMethod]
-        public void CadastroContaCorrenteControllerTeste()
+        public void CadastroContaCorrenteRepositoryTeste()
         {
-            repository = new CadastroContaCorrenteRepository();
-            var resultado = repository;
-            Assert.IsNotNull(resultado);
+            context = new SistemaContext();
+            Assert.IsNotNull(context);
         }
-        [HttpGet, Route("cadastroContaCorrente")]
-        public void  ObterPeloIdTeste(int id)
+        [TestMethod]
+        public void AlterarTeste()
         {
-            var retorno = Json(repository.ObterPeloId(id), JsonRequestBehavior.AllowGet);
-            Assert.IsNotNull(retorno);
+            var conta = new CadastroContaCorrente();
+            conta.Agencia = new Agencia();
+            conta.Id = 5154;
+            conta.NumeroConta = 10;
+            conta.RegistroAtivo = true;
+            
+            bool Alterar(CadastroContaCorrente cadastrosContaCorrente)
+            {
+                var cadastroContaCorrenteOriginal = conta;
+                if (cadastrosContaCorrente == null)
+                {
+                    return false;
+                }
+
+                cadastroContaCorrenteOriginal.IdAgencia = conta.Id;
+                cadastroContaCorrenteOriginal.NumeroConta = 11;
+                int quantidadeAfetada = 1;
+                return quantidadeAfetada == 1;
+
+            }
+            var resultado = Alterar(conta);
+            Assert.AreEqual(true, resultado);
         }
-        [HttpGet, Route("obtertodos")]
+
+        [TestMethod]
+        public void ApagarTeste()
+        {
+            var conta = new CadastroContaCorrente();
+            conta.Agencia = new Agencia();
+            conta.Id = 5154;
+            conta.NumeroConta = 10;
+            conta.RegistroAtivo = true;
+            bool Apagar(int id)
+            {
+                var CadastroContaCorrente = conta;
+                if (CadastroContaCorrente == null)
+                {
+                    return false;
+                }
+                CadastroContaCorrente.RegistroAtivo = false;
+                int quantidadeAfetada = 1;
+                return quantidadeAfetada == 1;
+            }
+            var resultado = Apagar(conta.Id);
+            Assert.AreEqual(true, resultado);
+
+        }
+
+        [TestMethod]
+        public void InserirTeste()
+        {
+            var conta = new CadastroContaCorrente();
+            conta.Agencia = new Agencia();
+            conta.Id = 5154;
+            conta.NumeroConta = 10;
+            conta.RegistroAtivo = true;
+            int Inserir(CadastroContaCorrente cadastrosContaCorrente)
+            {
+                cadastrosContaCorrente.RegistroAtivo = true;
+                context.CadastroContaCorrentes.Add(cadastrosContaCorrente);
+                context.SaveChanges();
+                return conta.Id;
+            }
+            var resultado = Inserir(conta);
+            Assert.AreEqual(conta.Id,resultado);
+        }
+
+        [TestMethod]
+        public CadastroContaCorrente ObterPeloIdTeste(int id)
+        {
+            var contacorrente = context.CadastroContaCorrentes
+                .Include("Agencia")
+               .FirstOrDefault(x => x.Id == id);
+            return contacorrente;
+        }
+
+        [TestMethod]
         public void ObterTodosTeste()
         {
-            var cadastroContaCorrente = repository.ObterTodos();
-            var resultado = new { data = cadastroContaCorrente };
-            var retorno =  Json(resultado, JsonRequestBehavior.AllowGet);
-            Assert.IsNotNull(retorno);
-        }
-        [HttpPost]
-        public void InserirTeste(CadastroContaCorrente cadastroContaCorrente)
-        {
-            cadastroContaCorrente.RegistroAtivo = true;
-            var id = repository.Inserir(cadastroContaCorrente);
-            var resultado = new { id = id };
-            var retorno =  Json(resultado);
-            Assert.IsNotNull(retorno);
-        }
-        [HttpPost, Route("cadastro")]
-        public void CadastroTeste(CadastroContaCorrente cadastroContaCorrente)
-        {
-            cadastroContaCorrente.RegistroAtivo = true;
-            int id = repository.Inserir(cadastroContaCorrente);
-            var resultado = new { id = id };
-            var retorno = RedirectToAction("Index", resultado);
-            Assert.IsNotNull(retorno);
-        }
-
-        [HttpPost]
-        public void UpdateTeste(CadastroContaCorrente cadastroContaCorrente)
-        {
-            var alterou = repository.Alterar(cadastroContaCorrente);
-            var resultado = new { status = alterou };
-            var retorno = Json(resultado);
-            Assert.IsNotNull(retorno);
-        }
-
-        [HttpGet, Route("apagar")]
-        public void ApagarTeste(int id)
-        {
-            var apagou = repository.Apagar(id);
-            var resultado = new { status = apagou };
-            var retorno = Json(resultado, JsonRequestBehavior.AllowGet);
-            Assert.IsNotNull(retorno);
-        }
-
-        public void IndexTeste()
-        {
-            var retorno = View();
-            Assert.IsNotNull(retorno);
-        }
-
-        [HttpGet, Route("Index")]
-        public void  CadastroTeste()
-        {
-            var retorno =  View();
-            Assert.IsNotNull(retorno);
+            var conta = new CadastroContaCorrente();
+            conta.Agencia = new Agencia();
+            conta.Id = 5154;
+            conta.NumeroConta = 10;
+            conta.RegistroAtivo = true;
+            List<CadastroContaCorrente> ObterTodos()
+            {
+                return context.CadastroContaCorrentes
+                    .Include("Agencia")
+                    .Where(x => x.RegistroAtivo == true).OrderBy(x => x.Id).ToList();
+            }
+            var resultado = ObterTodos();
+            Assert.IsNotNull(resultado);
         }
 
 
---
+++ FirstTestes/Testes/CaixaTeste.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using Repository;
+using Repository.Repositories;
+using TccFirst.Controllers;
+
+namespace FirstTestes.Testes
+{
+    [TestClass]
+    public class CaixaTeste : CaixaRepository
+    {
+        //SistemaContext context; // possivel erro
+
+        [TestMethod]
+        public void CaixaRepository()
+        {
+            var context = new SistemaContext();
+            Assert.IsNotNull(context);
+        }
+        [TestMethod]
+        public void AlterarTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+
+            bool Alterar(Caixa caixas)
+            {
+                var caixaRegistro = caixa;
+
+                if (caixaRegistro == null)
+                    return false;
+                caixaRegistro.IdHistoricos = caixas.IdHistoricos;
+                caixaRegistro.Operacao = "operação teste 2";
+                caixaRegistro.Descricao = "teste 2";
+                caixaRegistro.Documento = "teste 2";
+                caixaRegistro.FormaPagamento = "forma de pagamento";
+                caixaRegistro.Valor = 2000;
+                caixaRegistro.DataLancamento = DateTime.Now;
+                int quantidadeAfetada = 1;
+                return quantidadeAfetada == 1;
+            }
+            var resultado = Alterar(caixa);
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+        public void ApagarTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+            bool Apagar(int id)
+            {
+                var caixaTeste = caixa;
+                if (caixaTeste == null)
+                    return false;
+                caixaTeste.RegistroAtivo = false;
+                int quantidadeAfetada = 1;
+                return quantidadeAfetada == 1;
+            }
+            var resultado = Apagar(caixa.Id);
+            Assert.AreEqual(true, resultado);
+        }
+        [TestMethod]
+        public void InserirTeste()
+        {
+            var context = new SistemaContext();
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+            int Inserir(Caixa caixas)
+            {
+                context.Caixas.Add(caixa);
+                context.SaveChanges();
+                return caixas.Id;
+            }
+            var resultado = Inserir(caixa);
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public void ObterPeloIdTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+            var context = new SistemaContext();
+            Caixa ObterPeloId(int id)
+            {
+
+                var caixas = caixa;
+                return caixas;
+            }
+            var resultado = ObterPeloId(caixa.Id);
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public void ObterTodosTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+            List<Caixa> ObterTodos()
+            {
+                var context = new SistemaContext();
+                return context.Caixas.Include("Historico").Where(x => x.RegistroAtivo == true).OrderBy(x => x.Id).ToList();
+            }
+            var resultado = ObterTodos();
+            Assert.IsNotNull(resultado);
+        }
+        [TestMethod]
+        public void ObterTodosRelatorioTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+            List<Caixa> ObterTodosRelatorio(string dataInicial, string dataFinal, int idHistorico, string descricao, int valor)
+            {
+                var context = new SistemaContext();
+                if (dataInicial == "")
+                {
+                    dataInicial = null;
+                }
+                if (dataFinal == "")
+                {
+                    dataFinal = null;
+                }
+                var query = context
+                    .Caixas
+                    .Include("Historico")
+                    .Where(x => x.RegistroAtivo);
+
+                if (idHistorico != Caixa.FiltroSemHistorico)
+                {
+                    query = query.Where(x => x.IdHistoricos == idHistorico);
+                }
+                if (!string.IsNullOrEmpty(descricao))
+                {
+                    query = query.Where(x => x.Descricao.Contains(descricao));
+                }
+                if ((dataInicial != null) && (dataFinal != null))
+                {
+                    DateTime dataInicialConvertida = Convert.ToDateTime(dataInicial);
+                    DateTime dataFinalConvertida = Convert.ToDateTime(dataFinal);
+                    query = query.Where(x => x.DataLancamento == dataInicialConvertida || x.DataLancamento <= dataFinalConvertida);
+                }
+                if (valor != 0)
+                {
+                    query = query.Where(x => x.Valor == valor);
+                }
+
+                return query.ToList();
+            }
+            var resultado = ObterTodosRelatorio("2001-01-01", "2020-03-31", 1, "teste", 2000);
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public void ObterDadosSumarizadosTeste()
+        {
+            var caixa = new Caixa();
+            caixa.IdHistoricos = 1;
+            caixa.Operacao = "operação teste";
+            caixa.Descricao = "teste";
+            caixa.Documento = "teste";
+            caixa.FormaPagamento = "forma de pagamento";
+            caixa.Valor = 1000;
+            caixa.DataLancamento = DateTime.Now;
+
+            var dataHoje = DateTime.Now;
+            var dataAntiga =  new DateTime(2001,01,01);
+            var context = new SistemaContext();
+            List<FluxoCaixa> ObterDadosSumarizados(DateTime dataInicial, DateTime dataFinal)
+            {
+                return context.Database
+                    .SqlQuery<FluxoCaixa>(@"
+                    SELECT FORMAT(caixas.data_lancamento, 'yyyy-MM-dd') AS data,  SUM(valor) as valor
+                    FROM caixas 
+                    GROUP BY FORMAT(caixas.data_lancamento, 'yyyy-MM-dd')
+                    ").ToList();
+            }
+            var resultado = ObterDadosSumarizados(dataAntiga,dataHoje);
+            Assert.IsNotNull(resultado);
+        }
+    }
+}
+
